50b345b43fd7df1aec5c816f5405564be2ad79ea
SankrityaT <sthaku20@asu.edu>
2025-11-17 23:27:27 -0700

    Initial Commit

---
+++ navia-app/navia_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  navia_appApp.swift
+//  navia-app
+//
+//  Created by Sankritya Thakur on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct navia_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
